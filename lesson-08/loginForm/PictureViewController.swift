9eb96b937bec94098f8dad1f1075545ce3a96dc9
Vladimir Bes <Vladimir@mail.ru>
2020-03-02 07:12:17 +1000

    lesson-08

--- lesson-08/loginForm/PictureViewController.swift
+++ lesson-08/loginForm/PictureViewController.swift
@@ -14,169 +14,180 @@ class PictureViewController: UIViewController {
     
     
     @IBOutlet weak var nameFriendLabel: NSLayoutConstraint!
+    @IBOutlet weak var imagesFriendOne: UIImageView!
     @IBOutlet weak var imagesFriendTwo: UIImageView!
-    @IBOutlet weak var imagesFriendOne:UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        imagesFriendOne.isUserInteractionEnabled = true
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe(_:)))
         let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe(_:)))
         
         leftSwipe.direction = .left
         rightSwipe.direction = .right
-        
-        imagesFriendOne.isUserInteractionEnabled = true
         imagesFriendOne.addGestureRecognizer(leftSwipe)
         imagesFriendOne.addGestureRecognizer(rightSwipe)
-        imagesFriendOne.image = collectionImage.first
         
+        //
+        imagesFriendTwo.isUserInteractionEnabled = true
         let leftSwipe2 = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe2(_:)))
         let rightSwipe2 = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe2(_:)))
+        
         leftSwipe2.direction = .left
         rightSwipe2.direction = .right
-        
-        
         imagesFriendTwo.addGestureRecognizer(leftSwipe2)
         imagesFriendTwo.addGestureRecognizer(rightSwipe2)
         
         
         
         
+        imagesFriendOne.image = collectionImage.first
         
     }
+    
     
     
     func animateReduceOne () {
-        UIView.animate(withDuration: 1,
-                       delay: 0,
-                       animations: {
-                        
-                        self.imagesFriendOne.transform = .init(scaleX: 0.7, y: 0.7)
-                        self.imagesFriendOne.alpha = 0
-        })
-    }
-    
-    func reverceReduceOne () {
-        UIView.animate(withDuration: 1,
-                       delay: 0,
-                       animations: {
-                        
-                        self.imagesFriendOne.transform = .init(scaleX: 1, y: 1)
-                        self.imagesFriendOne.alpha = 1
-        })
-    }
-    
-    func animateSwipeOne () {
-        self.imagesFriendOne.transform = CGAffineTransform(translationX: +self.view.bounds.width, y: 0)
-        UIView.animate(withDuration: 1,
-                       delay: 0.2,
-                       animations: {
-                        self.imagesFriendOne.transform = .identity
-                        self.imagesFriendOne.alpha = 1
-        })
-    }
-    func reverceSwipeOne() {
-        UIView.animate(withDuration: 1){
-            self.imagesFriendOne.transform = CGAffineTransform(translationX: +self.view.bounds.width - 100, y: 0)
-            
-        }
-    }
-    
-    
-    func animateReduceTwo () {
-        UIView.animate(withDuration: 1,
-                       delay: 0,
-                       animations: {
-                        
-                        self.imagesFriendTwo.transform = .init(scaleX: 0.7, y: 0.7)
-                        self.imagesFriendTwo.alpha = 0
-        })
-    }
-    
-    func reverceReduceTwo () {
+        self.imagesFriendTwo.transform = CGAffineTransform(translationX: 500, y: 0)
+        self.imagesFriendTwo.transform = .init(scaleX: 0.8, y: 0.8)
+        print("убрать 1 картинку")
         
-        UIView.animate(withDuration: 1,
-                              delay: 0,
-                              animations: {
-                               
-                               self.imagesFriendTwo.transform = .init(scaleX: 1, y: 1)
-                               self.imagesFriendTwo.alpha = 1
-               })
+        
+        
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.imagesFriendOne.transform = CGAffineTransform(translationX: -400, y: 0)
+                self.imagesFriendOne.alpha = 0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
+                self.imagesFriendOne.transform = CGAffineTransform(translationX: 450, y: 0)
+                self.imagesFriendTwo.transform = .identity
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25, animations: {
+                self.imagesFriendOne.alpha = 1
+                
+            })
+        })
+    }
+    func animateReverseOne () {
+        self.imagesFriendTwo.transform = CGAffineTransform(translationX: -400, y: 0)
+        self.imagesFriendTwo.transform = .init(scaleX: 0.8, y: 0.8)
+        
+        print("1убрать 1 картинку")
+        
+        
+        
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.imagesFriendOne.transform = CGAffineTransform(translationX: 400, y: 0)
+                self.imagesFriendOne.alpha = 0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
+                self.imagesFriendOne.transform = CGAffineTransform(translationX: 450, y: 0)
+                self.imagesFriendTwo.transform = .identity
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25, animations: {
+                self.imagesFriendOne.alpha = 1
+                
+            })
+        })
     }
     
-    func animateSwipeTwo () {
-        self.imagesFriendTwo.transform = CGAffineTransform(translationX: +self.view.bounds.width, y: 0)
-        UIView.animate(withDuration: 1,
-                       delay: 0.2,
-                       animations: {
-                        self.imagesFriendTwo.transform = .identity
-                        self.imagesFriendTwo.isHidden = false
-                        self.imagesFriendTwo.alpha = 1
-        }  )
+    
+    func animateReduceTwo() {
+        self.imagesFriendOne.transform = .init(scaleX: 0.8, y: 0.8)
+        print("Убрать вторую картинку")
+        
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.imagesFriendTwo.transform = CGAffineTransform(translationX: -400, y: 0)
+                self.imagesFriendTwo.alpha = 0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
+                self.imagesFriendTwo.transform = CGAffineTransform(translationX: 450, y: 0)
+                self.imagesFriendOne.transform = .identity
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25, animations: {
+                self.imagesFriendTwo.alpha = 1
+            })
+        })
     }
-    func reverceSwipeTwo() {
-        UIView.animate(withDuration: 1){
-            self.imagesFriendTwo.transform = CGAffineTransform(translationX: +self.view.bounds.width - 100, y: 0)
-            
-        }
+    func animateReverseTwo () {
+        self.imagesFriendOne.transform = CGAffineTransform(translationX: -400, y: 0)
+        self.imagesFriendOne.transform = .init(scaleX: 0.8, y: 0.8)
+        
+        print("1убрать 1 картинку")
+        
+        
+        
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.imagesFriendTwo.transform = CGAffineTransform(translationX: 400, y: 0)
+                self.imagesFriendTwo.alpha = 0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
+                self.imagesFriendTwo.transform = CGAffineTransform(translationX: 450, y: 0)
+                self.imagesFriendOne.transform = .identity
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25, animations: {
+                self.imagesFriendTwo.alpha = 1
+                
+            })
+        })
     }
     
     
     var count = 0
-    var addOne = true
-    var addTwo = false
     
-    @objc func onSwipe(_ sender: UISwipeGestureRecognizer){
-        if sender.direction == .left {
-            print("1left")
+    @objc func onSwipe (_ sender: UISwipeGestureRecognizer){
+        switch sender.direction {
+        case .left:
             count += 1
             print(count)
             if count < collectionImage.count {
                 animateReduceOne()
-                animateSwipeTwo()
                 imagesFriendTwo.image = collectionImage[count]
+            }else{
+                count = collectionImage.count - 1
             }
-            
-        }else if sender.direction == .right {
+        case .right:
             count -= 1
-            print("1right")
             if count >= 0 && count < collectionImage.count {
-                reverceReduceOne()
-                reverceSwipeTwo()
-                imagesFriendOne.image = collectionImage[count]
+                animateReverseOne ()
+                imagesFriendTwo.image = collectionImage[count]
             }else {
                 count = 0
             }
+        default: break
         }
-    }
         
-        @objc func onSwipe2(_ sender: UISwipeGestureRecognizer){
-            
-            if sender.direction == .left {
-                print("2left")
-                count += 1
-                print(count)
-                if count < collectionImage.count{
-                    animateReduceTwo()
-                    animateSwipeOne()
-                    imagesFriendOne.image = collectionImage[count]
-                }else{
-                    count = 3
-                }
-                
-            }else if sender.direction == .right {
-                count -= 1
-                print("2right")
-                if count >= 0 && count < collectionImage.count {
-                    reverceReduceOne()
-                    reverceSwipeTwo()
-                    imagesFriendOne.image = collectionImage[count]
-                }
-            }else {
-                count = 0
+    }
+    @objc func onSwipe2 (_ sender: UISwipeGestureRecognizer){
+        switch sender.direction {
+        case .left:
+            count += 1
+            print(count)
+            if count < collectionImage.count {
+                animateReduceTwo()
+                imagesFriendOne.image = collectionImage[count]
+            }else{
+                count = collectionImage.count - 1
             }
+        case .right:
+            count -= 1
+            if count >= 0 && count < collectionImage.count{
+                animateReverseTwo()
+                imagesFriendOne.image = collectionImage[count]
+            }
+            else{
+                count = collectionImage.count - 1
+            }
+            
+        default: break
         }
+        
     }
     
-
+    
+}
